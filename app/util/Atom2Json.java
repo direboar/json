4f0584f4ef8ab3ab73d38320a36d599b85fa316c
minokuba <gzw04102@nifty.ne.jp>
2011-12-19 01:54:06 +0900

    フォーマッタ

--- app/util/Atom2Json.java
+++ app/util/Atom2Json.java
@@ -19,6 +19,7 @@ import com.google.gson.JsonPrimitive;
 
 /**
  * AtomをJson形式に変換するユーティリティクラス
+ * 
  * @author minokuba
  */
 public class Atom2Json {
@@ -27,14 +28,14 @@ public class Atom2Json {
 	 * atomで決まっている配列で扱わない要素名
 	 */
 	private static final String[] NON_ARRAY_ATTRIBUTE_NAMES = new String[] {
-			"email","generator", "icon", "id", "logo", "name", "published", "rights", "title",
-			"subtitle", "summary", "updated" ,"uri" };
+			"email", "generator", "icon", "id", "logo", "name", "published",
+			"rights", "title", "subtitle", "summary", "updated", "uri" };
 
 	/**
-	 *  atomのextensionElementについて、配列で扱わない要素名を管理する配列
+	 * atomのextensionElementについて、配列で扱わない要素名を管理する配列
 	 */
 	private List<String> customizedNonArrayAttributeNames = new ArrayList<String>();
-	
+
 	/**
 	 * JSONPのコールバック関数
 	 */
@@ -42,6 +43,7 @@ public class Atom2Json {
 
 	/**
 	 * JSONP形式で返却する場合に、callback関数名を指定する
+	 * 
 	 * @param callback
 	 */
 	public void setCallback(String callback) {
@@ -50,15 +52,16 @@ public class Atom2Json {
 
 	/**
 	 * atomのextensionElementについて、配列で扱わない要素名を設定する
+	 * 
 	 * @param elmentName
 	 */
 	public void addNonArrayElemName(String elmentName) {
 		customizedNonArrayAttributeNames.add(elmentName);
 	}
 
-
 	/**
 	 * 配列として扱うか？
+	 * 
 	 * @param elementName
 	 * @return
 	 */
@@ -76,7 +79,9 @@ public class Atom2Json {
 
 	/**
 	 * atomのDOMオブジェクトをJson文字列に変換する。callbackが設定されている場合は、JSONP形式で返却する。
-	 * @param document atomのDOMオブジェクト
+	 * 
+	 * @param document
+	 *            atomのDOMオブジェクト
 	 * @return　JSONP文字列
 	 */
 	public String toJson(Document document) {
@@ -91,7 +96,9 @@ public class Atom2Json {
 
 	/**
 	 * atomのDOMオブジェクトをJsonObject文字列に変換する。
-	 * @param document atonのDocumentオブジェクト
+	 * 
+	 * @param document
+	 *            atonのDocumentオブジェクト
 	 * @return JsonObject(feedに対応する）
 	 */
 	public JsonObject toJsonObject(Document document) {
@@ -109,8 +116,11 @@ public class Atom2Json {
 
 	/**
 	 * 指定したElementの子要素として持つElementを評価し、JsonObjectの属性に設定する。
-	 * @param parentJson　処理対象のJsonObject
-	 * @param parentElement parentJsonに対応するatomのElementオブジェクト
+	 * 
+	 * @param parentJson
+	 *            　処理対象のJsonObject
+	 * @param parentElement
+	 *            parentJsonに対応するatomのElementオブジェクト
 	 */
 	private void handleChildrenElement(JsonObject parentJson,
 			Element parentElement) {
@@ -122,9 +132,10 @@ public class Atom2Json {
 
 				JsonElement chilJson = toJsonElement(childElement);
 				String elementName = childElement.getTagName();
-				//複数回登場するElementの場合は配列として設定。そうでない場合は属性として設定。
+				// 複数回登場するElementの場合は配列として設定。そうでない場合は属性として設定。
 				if (isArray(childElement.getTagName())) {
-					JsonArray jsonArray = (JsonArray) parentJson.get(elementName);
+					JsonArray jsonArray = (JsonArray) parentJson
+							.get(elementName);
 					if (jsonArray == null) {
 						jsonArray = new JsonArray();
 						parentJson.add(elementName, jsonArray);
@@ -133,9 +144,10 @@ public class Atom2Json {
 				} else {
 					parentJson.add(elementName, chilJson);
 				}
-				
-				//子要素のElementより生成したJsonElementがJsonObjectの場合は、再帰的に子要素を評価する。
-				//TODO 実際はJsonObjectに変換されるケースとしては属性を持つが子要素がないケースも含まれるので、今の処理は処理効率上ちょっとイマイチ
+
+				// 子要素のElementより生成したJsonElementがJsonObjectの場合は、再帰的に子要素を評価する。
+				// TODO
+				// 実際はJsonObjectに変換されるケースとしては属性を持つが子要素がないケースも含まれるので、今の処理は処理効率上ちょっとイマイチ
 				if (chilJson instanceof JsonObject) {
 					handleChildrenElement((JsonObject) chilJson, childElement);
 				}
@@ -144,18 +156,24 @@ public class Atom2Json {
 	}
 
 	/**
-	 * XMLElementをJsonElementに変換する。
-	 * 型の決定ルール:
+	 * XMLElementをJsonElementに変換する。 型の決定ルール:
 	 * <ol>
-	 *  <li>Elementに属性がある場合はJsonObjectに変換する<li>
-	 *  <li>Elementの子ノードにElementがある場合は、JsonObject似変換する<li>
-	 *  <li>上記以外の場合は、Element内のテキストコンテンツを扱う文字列型として扱う<li>
+	 * <li>Elementに属性がある場合はJsonObjectに変換する
+	 * <li>
+	 * <li>Elementの子ノードにElementがある場合は、JsonObject似変換する
+	 * <li>
+	 * <li>上記以外の場合は、Element内のテキストコンテンツを扱う文字列型として扱う
+	 * <li>
 	 * </ol>
 	 * JsonObjectの場合の属性値決定ルール
 	 * <ol>
-	 *  <li>Elementに属性がある場合は、全てJsonObjectの属性として設定する。<li>
-	 *  <li>Elementの子ノードにElementがない場合は、Element内のテキストコンテンツをcontentという名前の属性でJsonObjectに設定する<li>
+	 * <li>Elementに属性がある場合は、全てJsonObjectの属性として設定する。
+	 * <li>
+	 * <li>Elementの子ノードにElementがない場合は、
+	 * Element内のテキストコンテンツをcontentという名前の属性でJsonObjectに設定する
+	 * <li>
 	 * </ol>
+	 * 
 	 * @param element
 	 * @return
 	 */
@@ -179,7 +197,7 @@ public class Atom2Json {
 				jsonObject.add(attr.getName(),
 						new JsonPrimitive(attr.getValue()));
 			}
-			if(!hasElement){
+			if (!hasElement) {
 				String content = element.getTextContent();
 				if (content != null && !content.trim().equals("")) {
 					jsonObject.add("content", new JsonPrimitive(content));
